462807abe0dba229d7f6bdcb8e6cca0c1ef34c5e
nyon <clarinet758@gmail.com>
2014-11-10 01:17:40 +0900

    msn2

--- msweather2014.py
+++ msweather2014.py
@@ -12,7 +12,6 @@ import urllib2
 http://ism1000ch.hatenablog.com/entry/2014/02/04/033533
 
 xml memo
-http://hikm.hatenablog.com/entry/20090206/1233950923
 http://weather.service.msn.com/data.aspx?src=vista&weadegreetype=C&culture=ja-JP&wealocations=wc:JAXX0013
 
 """
@@ -20,34 +19,55 @@ http://weather.service.msn.com/data.aspx?src=vista&weadegreetype=C&culture=ja-JP
 
 class Weather:
     day = [u'今日', u'明日']
-
+    key = ['temperature','low','high','skytextday','precip']
     def __init__(self, name, code):
         self.name = name
         self.code = code
+
+
+
+    def reg(self,quo):
+        sp = quo.split('=')[1]
+        sp = sp.replace("\"","")
+        return sp
+
+
+    def tweet(self, message):
+        message = message.encode('utf-8')
+        print message
+
+
+    def today(self,w):
+        cnt = 0
+        for i in w:
+            if key[0] in i:
+                temp = reg(self,i)
+                cnt+=1
+            elif key[3] in i:
+                sky = reg(self,i)
+                cnt+=1
+            elif key[4] in i:
+                pre = reg(self,i)
+                cnt+=1
+            if cnt >= 3:
+                t = u"%sの%sら辺の天候は%sで気温は%s度くらい 降水確率は%sくらい #%d" % (self.day[0], self.name, sky, temp, pre, x.microsecond)
+                tweet(self,t)
+                break
+
 
     def get_weather(self):
         ep = "http://weather.service.msn.com/data.aspx?src=vista&weadegreetype=C&culture=ja-JP&wealocations=wc:"
         t = urllib2.urlopen(ep+self.code)
         u = t.read()
-        self.v = u.split(' ')
-
+        v = u.split(' ')
         x = datetime.datetime.now()
-        dt = 0
         if x.hour % 2:
-            dt = 1
-        base = f['forecasts'][dt]
-        telop = base['telop']
-        if base['temperature']['max']==None:
-            tem_max = str(base['temperature']['max'])
+            today(self,v)
+#            pass
         else:
-            tem_max = str(base['temperature']['max']['celsius'])
-        if base['temperature']['min']==None:
-            tem_min = str(base['temperature']['min'])
-        else:
-            tem_min = str(base['temperature']['min']['celsius'])
-        message = u"%sの%sら辺の天候は%sで気温は最高%s度くらいで最低が%s度くらい #%d" % (self.day[dt], self.name, telop, tem_max, tem_min, x.microsecond)
-        message = message.encode('utf-8')
-        oat.tweet(message)
+            today(self,v)
+#        message = u"%sの%sら辺の天候は%sで気温は最高%s度くらいで最低が%s度くらい #%d" % (self.day[dt], self.name, telop, tem_max, tem_min, x.microsecond)
+#        oat.tweet(message)
 
 
 place = [Weather(u'東京都','JAXX003')]
