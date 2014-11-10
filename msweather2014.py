20440ad2e24732cdb9b903feb6f3809e1323e555
nyon <clarinet758@gmail.com>
2014-11-11 01:13:46 +0900

    tomorrow

--- msweather2014.py
+++ msweather2014.py
@@ -2,56 +2,71 @@
 # -*- coding: UTF-8 -*-
 # caution:Creating Middle
 import datetime
-import json
 import oat
 import time
 import urllib2
 
-"""
-xml memo
-http://weather.service.msn.com/data.aspx?src=vista&weadegreetype=C&culture=ja-JP&wealocations=wc:JAXX0013
-"""
 
 class Weather:
     day = [u'今日', u'明日']
-    key = ['temperature','low','high','skytextday','precip']
+    key = ['temperature', 'low', 'high', 'skytextday', 'precip']
 
     def __init__(self, name, code):
         self.name = name
         self.code = code
 
-
-    def reg(self,quo):
+    def reg(self, quo):
         sp = quo.split('=')[1]
-        sp = sp.replace("\"","")
+        sp = sp.replace("\"", "")
         sp = sp.decode('utf-8')
         return sp
 
-
     def tweet(self, message):
         message = message.encode('utf-8')
-        print message
+#        print message
         oat.tweet(message)
 
-
-    def today(self,w):
+    def today(self, w):
         cnt = 0
         for i in w:
             if self.key[0] in i:
                 temp = self.reg(i)
-                cnt+=1
+                cnt += 1
             elif self.key[3] in i:
                 sky = self.reg(i)
-                cnt+=1
+                cnt += 1
             elif self.key[4] in i:
                 pre = self.reg(i)
-                cnt+=1
+                cnt += 1
             if cnt >= 3:
-                print self.day[0], self.name, sky, temp, pre, self.xms
                 t = u"%sの%sら辺の天候は%sで気温は%s度くらい 降水確率は%s％くらい #%d" % (self.day[0], self.name, sky, temp, pre, self.xms)
                 self.tweet(t)
                 break
 
+    def tomorrow(self, w):
+        cnt = 0
+        s = p = 0
+        for i in w:
+            if self.key[1] in i:
+                low = self.reg(i)
+                cnt += 1
+            elif self.key[2] in i:
+                high = self.reg(i)
+                cnt += 1
+            elif self.key[3] in i and s < 2:
+                sky = self.reg(i)
+                s += 1
+                if s >= 2:
+                    cnt += 1
+            elif self.key[4] in i and p < 2:
+                pre = self.reg(i)
+                p += 1
+                if p >= 2:
+                    cnt += 1
+            if cnt >= 4:
+                t = u"%sの%sら辺の天候は%sで気温は最高%s度くらいで最低%s度くらい 降水確率は%s％くらい #%d" % (self.day[1], self.name, sky, high, low, pre, self.xms)
+                self.tweet(t)
+                break
 
     def get_weather(self):
         ep = "http://weather.service.msn.com/data.aspx?src=vista&weadegreetype=C&culture=ja-JP&wealocations=wc:"
@@ -61,12 +76,9 @@ class Weather:
         x = datetime.datetime.now()
         self.xms = x.microsecond
         if x.hour % 2:
-            self.today(v)
-#            pass
+            self.tomorrow(v)
         else:
             self.today(v)
-#        message = u"%sの%sら辺の天候は%sで気温は最高%s度くらいで最低が%s度くらい #%d" % (self.day[dt], self.name, telop, tem_max, tem_min, x.microsecond)
-#        oat.tweet(message)
 
 
 place = [Weather(u'熊本県', 'JAXX0043'),
